9c006aeff1e838fddbedd674ed58cd8c9b1c4f88
nikitashah9714 <40361657+nikitashah9714@users.noreply.github.com>
2024-10-14 22:25:28 +0530

    Added test

---
+++ SocialNetworkProject/src/test/java/com/project/SocialNetworkMainTest.java
@@ -0,0 +1,13 @@
+package com.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class SocialNetworkMainTest {
+    @Test
+    void contextLoads() {
+    }
+}
